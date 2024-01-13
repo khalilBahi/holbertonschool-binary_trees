5ad05a52256f4594a724cfae98a654201cd50275
khalilBahi <7753@holbertonstudents.com>
2024-01-13 13:28:33 +0100

    insert a node from left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -26,6 +26,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		parent->left->parent = new_node;
+		new_node->parent = parent;
 		new_node->left = parent->left;
 		parent->left = new_node;
 	}
