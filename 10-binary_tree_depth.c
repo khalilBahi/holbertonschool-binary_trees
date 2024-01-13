535c64422bbe8d970ec22cd1f27dd14733caa43a
khalilBahi <7753@holbertonstudents.com>
2024-01-13 16:14:47 +0100

    measure the deapth of the tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,10 +6,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
+
 	if (tree->parent == NULL || tree == NULL)
 	{
 		return (0);
 	}
+	else
+		depth = binary_tree_depth(tree->parent) + 1;
 
-	return (binary_tree_depth(tree->parent) + 1);
+	return (depth);
 }
